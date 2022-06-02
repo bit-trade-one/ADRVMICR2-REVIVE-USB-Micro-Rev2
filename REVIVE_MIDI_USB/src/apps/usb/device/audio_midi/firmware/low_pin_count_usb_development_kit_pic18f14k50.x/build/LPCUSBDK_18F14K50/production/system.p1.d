7161f35157513d79fc48b1554a8abe12271ec3f4
T-Oda-BTO <85532743+T-Oda-BTO@users.noreply.github.com>
2022-06-02 17:09:22 +0900

    Delete REVIVE_MIDI_USB directory

--- REVIVE_MIDI_USB/src/apps/usb/device/audio_midi/firmware/low_pin_count_usb_development_kit_pic18f14k50.x/build/LPCUSBDK_18F14K50/production/system.p1.d
+++
@@ -1,8 +0,0 @@
-build/LPCUSBDK_18F14K50/production/system.p1:  \
-system.c  \
-system.h  \
-../../../../../../bsp/low_pin_count_usb_development_kit/pic18f14k50/buttons.h  \
-../../../../../../bsp/low_pin_count_usb_development_kit/pic18f14k50/leds.h  \
-io_mapping.h  \
-fixed_address_memory.h  \
-../demo_src/usb_config.h 
