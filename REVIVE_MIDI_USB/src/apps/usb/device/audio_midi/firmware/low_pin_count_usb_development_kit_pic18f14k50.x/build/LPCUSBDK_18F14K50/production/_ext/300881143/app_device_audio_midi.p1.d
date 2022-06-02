8b046464440c5a4af47cef34503e6c916c6f0698
T-Oda-BTO <t.oda@bit-trade-one.co.jp>
2022-06-02 17:03:00 +0900

    作例 MIDI 追加

---
+++ REVIVE_MIDI_USB/src/apps/usb/device/audio_midi/firmware/low_pin_count_usb_development_kit_pic18f14k50.x/build/LPCUSBDK_18F14K50/production/_ext/300881143/app_device_audio_midi.p1.d
@@ -0,0 +1,15 @@
+build/LPCUSBDK_18F14K50/production/_ext/300881143/app_device_audio_midi.p1:  \
+../demo_src/app_device_audio_midi.c  \
+system.h  \
+../../../../../../bsp/low_pin_count_usb_development_kit/pic18f14k50/buttons.h  \
+../../../../../../bsp/low_pin_count_usb_development_kit/pic18f14k50/leds.h  \
+io_mapping.h  \
+fixed_address_memory.h  \
+../demo_src/usb_config.h  \
+../../../../../../framework/usb/inc/usb.h  \
+../../../../../../framework/usb/inc/usb_common.h  \
+../../../../../../framework/usb/inc/usb_ch9.h  \
+../../../../../../framework/usb/inc/usb_device.h  \
+../../../../../../framework/usb/inc/usb_hal.h  \
+../../../../../../framework/usb/inc/usb_hal_pic18.h  \
+../../../../../../framework/usb/inc/usb_device_midi.h 
---
+++ REVIVE_MIDI_USB/src/apps/usb/device/audio_midi/firmware/low_pin_count_usb_development_kit_pic18f14k50.x/build/LPCUSBDK_18F14K50/production/_ext/300881143/app_led_usb_status.p1.d
@@ -0,0 +1,12 @@
+build/LPCUSBDK_18F14K50/production/_ext/300881143/app_led_usb_status.p1:  \
+../demo_src/app_led_usb_status.c  \
+system.h  \
+../../../../../../bsp/low_pin_count_usb_development_kit/pic18f14k50/buttons.h  \
+../../../../../../bsp/low_pin_count_usb_development_kit/pic18f14k50/leds.h  \
+io_mapping.h  \
+fixed_address_memory.h  \
+../demo_src/usb_config.h  \
+../../../../../../framework/usb/inc/usb_device.h  \
+../../../../../../framework/usb/inc/usb_common.h  \
+../../../../../../framework/usb/inc/usb_hal.h  \
+../../../../../../framework/usb/inc/usb_hal_pic18.h 
